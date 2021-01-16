815f7b2b04bad609d421f952aff7b2acf2541a3f
bjorn <nomail@mail.com>
2021-01-16 16:58:33 +0100

    terrain sohuld be ok. Something is wrong in StashElement.cs

--- Core/PoEMemory/MemoryObjects/TerrainDataObject.cs
+++ Core/PoEMemory/MemoryObjects/TerrainDataObject.cs
@@ -8,14 +8,14 @@ namespace ExileCore.PoEMemory.MemoryObjects
 
         public long Columns => M.Read<long>(Address + 0x18);
         public long Rows => M.Read<long>(Address + 0x20);
-        public int BytesPerRow => M.Read<int>(Address + 0x108);
+        public int BytesPerRow => M.Read<int>(Address + 0xE0);
 
-        public long P1Start => M.Read<long>(Address + 0xD8);
-        public long P1End => M.Read<long>(Address + 0xD8 + 8);
+        public long P1Start => M.Read<long>(Address + 0xB0);
+        public long P1End => M.Read<long>(Address + 0xB0 + 8);
         public byte[] MeleeLayerPathfindinData => M.ReadMem(P1Start, (int)(P1End - P1Start));
 
-        public long P2Start => M.Read<long>(Address + 0xF0);
-        public long P2End => M.Read<long>(Address + 0xF0 + 8);
+        public long P2Start => M.Read<long>(Address + 0xC8);
+        public long P2End => M.Read<long>(Address + 0xC8 + 8);
         public byte[] RangedLayerPathfindinData => M.ReadMem(P2Start, (int)(P2End - P2Start));
     }
 }
